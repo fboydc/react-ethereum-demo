49e48c071eeb40e6e34d8b0e99d5ca4d7fbb161e
fboydc <fboydc@gmail.com>
2018-11-24 08:07:48 -0500

    my first commit

---
+++ contracts/Pool.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.21;
+ 
+import "./iPool.sol";
+import "./Timed.sol";
+import "./iFishToken.sol";
+import "./FishToken.sol";
+import "node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract Pool is iPool, Timed {
+    using SafeMath for uint256;
+
+    string public name;
+    address public token;
+    uint256 public rate;
+
+    constructor(string _name, uint256 _rate,  uint256 _deadline) public{
+        require(_rate > 0);
+        require(_deadline > block.timestamp);
+        name = _name;
+        rate = _rate;
+        deadline = _deadline;
+        token = new FishToken(_deadline);
+    }
+
+    function() public payable onlyWhileOpen {
+        require(msg.value > 0);
+        uint256 rewardTokens = rate.mul(msg.value);
+        require(iFishToken(token).issueTokens(msg.sender, rewardTokens));
+    }
+
+    function withdraw() public onlyWhileClosed returns (bool success) {
+        if(iFishToken(token).isShark(msg.sender)) {
+            msg.sender.transfer(address(this).balance);
+            return true;
+        }
+
+        return false;
+    } 
+
+}
---
+++ contracts/iPool.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.21;
+
+contract iPool {
+
+    function() public payable;
+
+    function withdraw() public returns (bool success);
+
+}
